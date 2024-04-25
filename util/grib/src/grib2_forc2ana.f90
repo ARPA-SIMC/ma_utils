eface788f032833f24096b99df890d6f8d5606a6
Enrico Minguzzi <eminguzzi@Minguzzi-LP-SMR.smr.arpa.emr.net>
2024-04-25 15:21:50 +0200

    Added templates for arkimet queries for Icon; updated program grib2_forc2ana.exe

--- util/grib/src/grib2_forc2ana.f90
+++ util/grib/src/grib2_forc2ana.f90
@@ -2,12 +2,13 @@ PROGRAM grib_forc2ana
 !--------------------------------------------------------------------------
 ! Legge un file con molti grib (edizione 2) lo riscrive come analisi.
 !
-! Note:
-! La chiave productDefinitionTemplateNumber viene forzata  0 (istantaneo)
-! oppure 8 (eaborato), perdendo le informazioni accessorie (ens, chimica, etc.)
-! Questo potrebbe essere reso opzionale.  
+! Nota 25/04/2024:
+! nei campi Kenda istantanei, productDefinitionTemplateNumber=1 (individual ensemble forecast; table 4.0).
+! Se la forzo a 0 (analisi o forecast), le chiavi relative ai livelli delle nubi vengono corrotte.
+! Potrebbe essere un bug della versione corrente di grib-api, ma nel dubbio riduco al minimo le
+! modifiche a questa chiave, anche se l'archivio iama risualta meno omogeneo...
 !
-!                                         Versione 1.0.0, Enrico 24/04/2024
+!                                         Versione 1.0.0, Enrico 25/04/2024
 !--------------------------------------------------------------------------
 
 USE datetime_class
@@ -20,11 +21,13 @@ INTEGER :: kg,idp,kp,iret,yy,mon,dd,hh,min,en,pdtn,iouotr,iouftr,ft,lotr
 INTEGER :: cnt_ist,cnt_proc,fth,pdtn_out,lotrh
 CHARACTER(LEN=200) :: filein,fileout,chdum
 CHARACTER(LEN=4) :: tr_proc
+LOGICAL :: force_tmpl
 
 !--------------------------------------------------------------------------
 ! 1) Preliminari
 
 ! 1.1 Parametri da riga comando
+force_tmpl = .FALSE.
 tr_proc = "proc"
 idp = 0
 DO kp = 1,HUGE(0)
@@ -38,6 +41,8 @@ DO kp = 1,HUGE(0)
     tr_proc = "isti"
   ELSE IF (TRIM(chdum) == "-istf") THEN
     tr_proc = "istf"
+  ELSE IF (TRIM(chdum) == "-ftmp") THEN
+    force_tmpl = .TRUE.
 
   ELSE 
     idp = idp + 1
@@ -77,6 +82,7 @@ grib: DO kg = 1,HUGE(0)
   IF ( en /= 2 ) THEN
     WRITE (*,*) "Trovato campo grib1, scrivo invariato"
     CALL grib_write (igin,ifout)
+    CALL grib_release(igin)
     CYCLE grib
   ENDIF
     
@@ -120,9 +126,8 @@ grib: DO kg = 1,HUGE(0)
     CALL grib_set(igout,"day",dd)
     CALL grib_set(igout,"hour",hh)
     CALL grib_set(igout,"minute",min)
-
-    CALL grib_set(igout,"productDefinitionTemplateNumber",0)   ! table 4.0
-
+    IF (force_tmpl) CALL grib_set(igout,"productDefinitionTemplateNumber",0) ! table 4.0
+    
   ELSE IF (pdtn == 8 .OR. pdtn == 11 .OR. pdtn == 42 .OR. pdtn == 43) THEN  
     cnt_proc = cnt_proc + 1
 
@@ -136,7 +141,7 @@ grib: DO kg = 1,HUGE(0)
       CALL grib_set(igout,"hour",hh)
       CALL grib_set(igout,"minute",min)
 
-      CALL grib_set(igout,"productDefinitionTemplateNumber",8) ! table 4.0
+      IF (force_tmpl) CALL grib_set(igout,"productDefinitionTemplateNumber",8) ! table 4.0
       CALL grib_set(igout,"typeOfTimeIncrement",1)             ! table 4.11
 
 ! 2.5.3 Campo elaborato: trange="istantaneo", reftime="inizio intervallo", forecastTime=0, 
@@ -150,13 +155,17 @@ grib: DO kg = 1,HUGE(0)
       CALL grib_set(igout,"minute",min)
 
       CALL grib_set(igout,"typeOfStatisticalProcessing",255)
-      CALL grib_set(igout,"productDefinitionTemplateNumber",0)
-!     IF (pdtn == 8) pdtn_out = 0
-!     IF (pdtn == 11) pdtn_out = 1
-!     IF (pdtn == 42) pdtn_out = 40
-!     IF (pdtn == 43) pdtn_out = 41
-!     CALL grib_set(igout,"productDefinitionTemplateNumber",pdtn_out)
 
+      IF (force_tmpl) THEN
+        pdtn_out = 0
+      ELSE
+        IF (pdtn == 8) pdtn_out = 0
+        IF (pdtn == 11) pdtn_out = 1
+        IF (pdtn == 42) pdtn_out = 40
+        IF (pdtn == 43) pdtn_out = 41
+      ENDIF
+      CALL grib_set(igout,"productDefinitionTemplateNumber",pdtn_out)
+      
 ! 2.5.4 Campo elaborato: trange=istantaneo, reftime="fine intervallo", forecastTime=0,
     ELSE IF (tr_proc == "istf") THEN
       CALL grib_get(igin,"lengthOfTimeRange",lotr)
@@ -179,12 +188,16 @@ grib: DO kg = 1,HUGE(0)
       CALL grib_set(igout,"minute",min)
 
       CALL grib_set(igout,"typeOfStatisticalProcessing",255)
-      CALL grib_set(igout,"productDefinitionTemplateNumber",0)
-!     IF (pdtn == 8) pdtn_out = 0
-!     IF (pdtn == 11) pdtn_out = 1
-!     IF (pdtn == 42) pdtn_out = 40
-!     IF (pdtn == 43) pdtn_out = 41
-!     CALL grib_set(igout,"productDefinitionTemplateNumber",pdtn_out)
+
+      IF (force_tmpl) THEN
+        pdtn_out = 0
+      ELSE
+        IF (pdtn == 8) pdtn_out = 0
+        IF (pdtn == 11) pdtn_out = 1
+        IF (pdtn == 42) pdtn_out = 40
+        IF (pdtn == 43) pdtn_out = 41
+      ENDIF
+      CALL grib_set(igout,"productDefinitionTemplateNumber",pdtn_out)
 
     ENDIF
 
@@ -194,7 +207,9 @@ grib: DO kg = 1,HUGE(0)
   ENDIF
 
   CALL grib_write (igout,ifout)
-
+  CALL grib_release (igin)
+  CALL grib_release (igout)
+  
 ENDDO grib
 
 !--------------------------------------------------------------------------
@@ -239,13 +254,17 @@ SUBROUTINE write_help
 ! Scrive a schermo l'help del programma
 
 !            123456789012345678901234567890123456789012345678901234567890123456789012345
-WRITE (*,*) "Uso: grib2_forc2ana.exe [-h] filein fileout [-isti / -istf]"
+WRITE (*,*) "Uso: grib2_forc2ana.exe [-h] filein fileout [-isti/-istf] [-ftmp]"
 WRITE (*,*) "Legge un file con molti grib (edizione 2) e lo riscrive come analisi"
 WRITE (*,*) "I campi non istantnei sono scritti come:"
 WRITE (*,*) "  [default]: analisi elaborate, con reftime=inizio intervallo di elaborazione"
 WRITE (*,*) "  [-isti]: analisi istantanee, con reftime=inizio intervallo di elaborazione"
 WRITE (*,*) "  [-istf]: analisi istantanee, con reftime=fine intervallo di elaborazione"
-WRITE (*,*) ""
+WRITE (*,*) "  [-ftmp]: forza la chiave grib ""productDefinitionTemplateNumber"" a 0 (ist)"
+WRITE (*,*) "           oppure 8 (proc). Rende i dati di output piu' omogenei, ma in rari"
+WRITE (*,*) "           casi corrompe altre chbiavi dei grib (es. livelli delle nubi su"
+WRITE (*,*) "           strati, livelli nel suolo). Usare con cautela"
+WRITE (*,*)
 !            123456789012345678901234567890123456789012345678901234567890123456789012345
 
 RETURN
